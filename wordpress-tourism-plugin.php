c81afa00dd0fce1af799c89df206e5c4ae9d5471
galleanosebastian-droid <galleano.sebastian@gmail.com>
2026-03-06 17:54:55 -0300

    Add initial WordPress tourism plugin scaffold

---
+++ wordpress-tourism-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WordPress Tourism Plugin
+ * Description: Starter plugin scaffold for tourism features.
+ * Version: 0.1.0
+ * Author: Your Name
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
